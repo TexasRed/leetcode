0086b5ee9af7e5d330055f7d65ad56882e4b0e7e
Kai Huang <huangkai1988@gmail.com>
2016-09-30 00:10:38 -0700

    [2. Add Two Numbers][Accepted]committed by LeetCode Extension

--- 2-Add-Two-Numbers/solution.java
+++ 2-Add-Two-Numbers/solution.java
@@ -14,30 +14,20 @@ public class Solution {
         ListNode header = new ListNode(-1);
         ListNode p1 = l1, p2 = l2, tail = header;
         int carrier = 0;
-        while (p1 != null && p2 != null) {
-            int sum = (p1.val + p2.val + carrier);
+        while (p1 != null || p2 != null) {
+            int v1 = (p1 != null) ? p1.val : 0;
+            int v2 = (p2 != null) ? p2.val : 0;
+            int sum = (v1 + v2 + carrier);
             int digit = sum % 10;
             carrier = sum / 10;
             tail.next = new ListNode(digit);
             tail = tail.next;
-            p1 = p1.next;
-            p2 = p2.next;
-        }
-        while (p1 != null) {
-            int sum = (p1.val + carrier);
-            int digit = sum % 10;
-            carrier = sum / 10;
-            tail.next = new ListNode(digit);
-            tail = tail.next;
-            p1 = p1.next;
-        }
-        while (p2 != null) {
-            int sum = (p2.val + carrier);
-            int digit = sum % 10;
-            carrier = sum / 10;
-            tail.next = new ListNode(digit);
-            tail = tail.next;
-            p2 = p2.next;
+            if (p1 != null) {
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                p2 = p2.next;
+            }
         }
         if (carrier > 0) {
             tail.next = new ListNode(carrier);
