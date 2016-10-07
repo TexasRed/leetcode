42840e876817aff7ee8963dcd67436010efdfdc0
Kai Huang <huangkai1988@gmail.com>
2016-10-07 00:42:17 -0700

    [346. Moving Average from Data Stream][Accepted]committed by LeetCode Extension

--- 346-Moving-Average-from-Data-Stream/solution.java
+++ 346-Moving-Average-from-Data-Stream/solution.java
@@ -1,35 +1,22 @@
 public class MovingAverage {
-    int start;
-    int end;
-    int[] ring_buffer;
     int sum;
-    int size;
+    int capacity;
+    Deque<Integer> deque;
     
     /** Initialize your data structure here. */
     public MovingAverage(int size) {
-        this.sum = 0;
-        this.ring_buffer = new int[size];
-        this.size = size;
-        this.start = -1;
-        this.end = 0;
+        this.deque = new LinkedList<>();
+        this.capacity = size;
     }
     
     public double next(int val) {
-        if (start == end) {
-            sum = sum - ring_buffer[start] + val;
-            ring_buffer[start] = val;
-            start = (start + 1) % size;
-            end = start;
-            return (double) sum / size;
+        deque.offer(val);
+        sum += val;
+        if (deque.size() <= capacity) {
+            return (double) sum / deque.size();
         } else {
-            sum += val;
-            if (start == -1) {
-                start += 1;
-            }
-            ring_buffer[end] = val;
-            int len = end - start + 1;
-            end = (end + 1) % size;
-            return (double) sum / len;
+            sum -= deque.pollFirst();
+            return (double) sum / capacity;
         }
     }
 }
